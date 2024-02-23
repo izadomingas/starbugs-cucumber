eaef84d1bcba32fb74533f1b2974cab814560b6f
Izaide <izaide.tenutes@loglabdigital.com.br>
2024-02-23 13:44:49 -0400

    Configurações de Relatório

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -10,3 +10,14 @@ Before  do
    @order = OrderPage.new
    @popup = Popup.new
 end
+
+After do |scenario|
+
+   screenshot = page.save_screenshot("logs/screenshots/#{scenario.__id__}.png")
+   attach(screenshot, "image/png", "Screenshot")
+
+   # if (scenario.failed?)
+   #     screenshot = page.save_screenshot("logs/screenshots/temp.png")
+   #     attach(screenshot, "image/png", "Screenshot")
+   # end
+end
